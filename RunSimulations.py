355cc3f7376b0bc9bebe1af1d3590bb45b2f95af
Heber Rocha <heberonly@gmail.com>
2024-01-25 16:08:12 -0500

    Update RunSimulations.py
    - clean version

--- RunSimulations.py
+++ RunSimulations.py
@@ -11,23 +11,13 @@ if __name__ == '__main__':
     PhysiCell_Model, Samples, Replicates = args_run_simulations(sys.argv[1:])
     NumSimulations = len(Samples)
     NumSimulationsPerRank  = int(NumSimulations/size)
-    mod = NumSimulations%size
     
-    if ( mod != 0): NumSimulationsPerRank = NumSimulationsPerRank + 1 # if there is no equal split on the nodes, add a ghost simulation
-    data = None
+    print(f"Total number of simulations: {NumSimulations} Simulations per rank: {NumSimulationsPerRank}")
 
-    if rank == 0:
-        print(f"Total number of simulations: {NumSimulations} Simulations per rank: {NumSimulationsPerRank}")
-        data = np.arange(NumSimulations) # [0,1,...,NumSimulations-1]
-        if ( mod != 0):
-          add = -1*np.ones((size-mod),dtype='d') # Add -1 in the ghost simulations
-          data = np.concatenate((data,add),axis=None, dtype='d') # now len(data) % size == 0 
+    data = np.array_split(np.arange(NumSimulations),size, axis=0) # split [0,1,...,NumSimulations-1] in size arrays equally (or +1 in some ranks)
 
-    recvbuf = np.empty(NumSimulationsPerRank, dtype='d') # These are the indexes of samples NOT the samples number
-    comm.Scatter(data, recvbuf, root=0)
-    for ind_sim in recvbuf:
-        if ( ind_sim < 0 ): continue # If recvbuf is negative (-1) do NOT execute the model
-        sampleID = Samples[int(ind_sim)]
-        replicateID = Replicates[int(ind_sim)]
+    for ind_sim in data[rank]:
+        sampleID = Samples[ind_sim]
+        replicateID = Replicates[ind_sim]
         print('Rank: ',rank, ', Simulation: ', ind_sim, ', Sample: ', sampleID,', Replicate: ', replicateID)
-        # model(PhysiCell_Model.get_configFilePath(sampleID, replicateID), PhysiCell_Model.executable)
+        model(PhysiCell_Model.get_configFilePath(sampleID, replicateID), PhysiCell_Model.executable)
