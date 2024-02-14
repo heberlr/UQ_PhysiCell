63822f230ae40e5b99c061f63cf09c3cd4868cb5
Heber Rocha <heberonly@gmail.com>
2024-02-14 15:16:00 -0500

    CalibExample

--- main_calib.py
+++ main_calib.py
@@ -16,7 +16,6 @@ PhysiCellModel1 = PhysiCell_Model(fileName, key_model)
 NumCoresPYMC = 2 # If NumCoresPYMC = 1 is serial, else in parallel
 
 def sum_stat(output_folders):
-    return np.array([999.0, 11])
     dic_output = {'liveCells': [], 'deadCells': []}
     for outputFolder in output_folders:
         mcds = pcdl.TimeStep('output00000048.xml',outputFolder, microenv=False, graph=False, settingxml=None)
@@ -39,7 +38,7 @@ def PhysiCell_model(rng, *args, size=None):
     # Run the replicates
     output_folders = []
     for replicateID in range(PhysiCellModel1.numReplicates):
-        # model(PhysiCellModel1.get_configFilePath(SampleID, replicateID), PhysiCellModel1.executable)
+        model(PhysiCellModel1.get_configFilePath(SampleID, replicateID), PhysiCellModel1.executable)
         os.remove( pathlib.Path(PhysiCellModel1.get_configFilePath(SampleID, replicateID)) ) # remove config file XML
         output_folders.append(PhysiCellModel1.get_outputPath(SampleID, replicateID))
 
@@ -47,28 +46,33 @@ def PhysiCell_model(rng, *args, size=None):
     # print(f"... running PhysiCell")
     return sum_stat(output_folders)
 
+def Run_CalibrationSMC(observedData, FileNameInfData):
+    with pm.Model() as model_lv:
+        par1 = pm.HalfNormal("viral_replication_rate", 0.15) # Positive values
+        par2 = pm.Uniform("min_virion_count", lower = 0, upper = 100) # Positive values
 
-if __name__ == '__main__':
-    print(f"Running on PyMC v{pm.__version__}")
-    # To load data: 
-    idata_lv = az.from_netcdf("InfData_.nc")
-    print(idata_lv['posterior'], idata_lv['sample_stats'], idata_lv['observed_data'])
-    print( f"Chain: {idata_lv['sample_stats']['log_marginal_likelihood']['chain']}, Draw: {idata_lv['sample_stats']['log_marginal_likelihood']['draw']}")
+        sim = pm.Simulator("sim", PhysiCell_model, params=[par1, par2], epsilon=100, observed=observedData)
+
+        idata_lv = pm.sample_smc(draws=100, chains=10, cores=NumCoresPYMC, return_inferencedata = False, idata_kwargs={'log_likelihood':True} ) # return inference data (return_inferencedata=True is default pymc3 > 4.0)
+        idata_lv.to_netcdf(FileNameInfData) # save the inferencedata
+
+def Plot_Calibration(FileNameInfData):
+    idata_lv = az.from_netcdf(FileNameInfData)
+    # print(idata_lv['posterior'], idata_lv['sample_stats'], idata_lv['observed_data'])
+    print( f"""Chain: {idata_lv['sample_stats']['log_marginal_likelihood']['chain']})
+          Draw: {idata_lv['sample_stats']['log_marginal_likelihood']['draw']}""")
     # print( idata_lv['sample_stats']['beta'])
     # print( idata_lv['sample_stats']['accept_rate'])
-    exit(0)
-    data_avg_live_dead = np.array([1000, 10]) # observational data
-    # call the rng number + parameters
-    # PhysiCell_model(0, 1.2, 2.1, 1) # Test of function
-    with pm.Model() as model_lv:
-        par1 = pm.HalfNormal("viral_replication_rate", 1.0) # Positive values
-        par2 = pm.HalfNormal("min_virion_count", 5.0) # Positive values
+    az.plot_trace(idata_lv, kind="rank_vlines")
+    az.plot_posterior(idata_lv)
+    plt.show()
 
-        sim = pm.Simulator("sim", PhysiCell_model, params=[par1, par2], epsilon=100, observed=data_avg_live_dead)
+if __name__ == '__main__':
+    # print(f"Running on PyMC v{pm.__version__}")
+    FileNameInfData = "InfData_.nc"
+    data_avg_live_dead = np.array([50, 0]) # observational data
+    Run_CalibrationSMC(data_avg_live_dead, FileNameInfData)
+    # Plot_Calibration(FileNameInfData)
 
-        idata_lv = pm.sample_smc(draws=1000, chains=2, cores=NumCoresPYMC, idata_kwargs={'log_likelihood':True} ) # return inference data (return_inferencedata=True is default pymc3 > 4.0)
-        idata_lv.to_netcdf("InfData_.nc") # save the inferencedata
 
-        az.plot_trace(idata_lv, kind="rank_vlines")
-        az.plot_posterior(idata_lv)
-        plt.show()
+        
