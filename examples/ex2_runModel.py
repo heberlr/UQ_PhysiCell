2daa0162e09f2f1e87bb7721af3c191e479bdbf9
Heber Lima da Rocha <heberonly@gmail.com>
2024-08-15 12:15:38 -0400

    new name of sum_stats fucntion

--- examples/ex2_runModel.py
+++ examples/ex2_runModel.py
@@ -1,5 +1,5 @@
 from uq_physicell.uq_physicell import PhysiCell_Model
-from uq_physicell.sumstats import summ_func
+from uq_physicell.sumstats import summ_func_FinalPopLiveDead
 import numpy as np
 
 if __name__ == '__main__':
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     # Create the structure of model exploration
     PhysiCellModel = PhysiCell_Model(fileName, key_model)
     # Print information of exploration
-    PhysiCellModel.RunModel(SampleID=0, ReplicateID=0,Parameters=np.array([0.75,0.5]),SummaryFunction=summ_func)
+    PhysiCellModel.RunModel(SampleID=0, ReplicateID=0,Parameters=np.array([0.75,0.5]),SummaryFunction=summ_func_FinalPopLiveDead)
     PhysiCellModel.RunModel(SampleID=0, ReplicateID=1,Parameters=np.array([0.75,0.5]),RemoveConfigFile=False)
-    PhysiCellModel.RunModel(SampleID=1, ReplicateID=0,Parameters=np.array([0.80,0.55]), SummaryFunction=summ_func)
+    PhysiCellModel.RunModel(SampleID=1, ReplicateID=0,Parameters=np.array([0.80,0.55]), SummaryFunction=summ_func_FinalPopLiveDead)
 
