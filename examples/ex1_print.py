242791c99778024ac50b2843ebc3f07c5927cbdd
bue <epbucher@iu.edu>
2025-03-20 20:00:31 -0400

    @ uq_physicell : fix __init__.py

--- examples/ex1_print.py
+++ examples/ex1_print.py
@@ -1,10 +1,10 @@
-from uq_physicell.uq_physicell import PhysiCell_Model
+from uq_physicell import PhysiCell_Model
 
 if __name__ == '__main__':
     fileName = "examples/SampleModel.ini"
     key_model = "physicell_model"
     key_model2 = "physicell_model_2"
-    
+
     # Create the structure of model exploration
     print("Generate the model exploration structure 1.")
     PhysiCellModel = PhysiCell_Model(fileName, key_model)
